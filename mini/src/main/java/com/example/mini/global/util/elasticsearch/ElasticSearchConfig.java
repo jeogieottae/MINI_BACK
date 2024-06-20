80016b3be7ed0dc0b869e76d9c981adbe81c093f
EomHyeYeong <hy30051@naver.com>
2024-06-20 14:47:47 +0900

    feat: elasticsearh 설정 추가

---
+++ mini/src/main/java/com/example/mini/global/util/elasticsearch/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package com.example.mini.global.util.elasticsearch;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.data.elasticsearch.url}")
+    private String url;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(url)
+                .build();
+    }
+}
