0dffe9acb3834724e8a9098f59e1ad39c449619c
leeshinbi <tlsql6831@naver.com>
2024-06-27 02:04:03 +0900

    redis 값 수정

--- mini/src/main/java/com/example/mini/global/redis/RedissonConfig.java
+++ mini/src/main/java/com/example/mini/global/redis/RedissonConfig.java
@@ -3,30 +3,23 @@ package com.example.mini.global.redis;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
-  @Value("${spring.data.redis.host}")
-  private String redisHost;
-
-  @Value("${spring.data.redis.port}")
-  private int redisPort;
-
-  @Value("${spring.data.redis.password}")
-  private String redisPassword;
-
+  private static final String REDIS_HOST = "redis"; // 혹은 직접 IP 주소 입력
+  private static final int REDIS_PORT = 6379;
+  private static final String REDIS_PASSWORD = "1234";
   private static final String REDISSON_HOST_PREFIX = "redis://";
 
   @Bean
   public RedissonClient redissonClient() {
     Config config = new Config();
     config.useSingleServer()
-        .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
-        .setPassword(redisPassword);
+        .setAddress(REDISSON_HOST_PREFIX + REDIS_HOST + ":" + REDIS_PORT)
+        .setPassword(REDIS_PASSWORD);
     return Redisson.create(config);
   }
 }
