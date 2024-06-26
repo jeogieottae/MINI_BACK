9b00790b32fd22077adbd738d567e142778ae85d
leeshinbi <tlsql6831@naver.com>
2024-06-26 20:27:50 +0900

    다시 push

--- mini/src/main/java/com/example/mini/global/redis/RedisConfig.java
+++ mini/src/main/java/com/example/mini/global/redis/RedisConfig.java
@@ -13,7 +13,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-
 	@Value("${spring.data.redis.host}")
 	private String redisHost;
 
