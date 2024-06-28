3dc7ef337da61498ced9d790954820c6d306ef34
leeshinbi <tlsql6831@naver.com>
2024-06-28 17:36:57 +0900

    REFACTOR : 불필요한 코드 삭제

--- mini/src/main/java/com/example/mini/domain/cart/controller/CartController.java
+++ mini/src/main/java/com/example/mini/domain/cart/controller/CartController.java
@@ -19,14 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/cart")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {
-    "http://localhost:8080",
-    "http://3.38.1.70:8080",
-    "http://ec2-3-38-1-70.ap-northeast-2.compute.amazonaws.com:8080",
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "https://127.0.0.1:3000"
-})
 public class CartController {
 
   private final CartService cartService;
