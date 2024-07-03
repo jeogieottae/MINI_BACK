17be2977cee722c7aba15ed134088f22df35bd77
leeshinbi <tlsql6831@naver.com>
2024-07-03 23:48:37 +0900

    Refactor : 전체 숙소 목록 조회에 이미지 반환 추가

--- mini/src/main/java/com/example/mini/domain/accomodation/model/response/AccomodationCardResponseDto.java
+++ mini/src/main/java/com/example/mini/domain/accomodation/model/response/AccomodationCardResponseDto.java
@@ -1,6 +1,9 @@
 package com.example.mini.domain.accomodation.model.response;
 
 import com.example.mini.domain.accomodation.entity.Accomodation;
+import com.example.mini.domain.accomodation.entity.AccomodationImage;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -23,28 +26,33 @@ public class AccomodationCardResponseDto {
     private Integer minPrice;
     private boolean reservationAvailable;
     private int likeCount;
+    private List<String> imageUrls;
 
-    public static AccomodationCardResponseDto toDto(Accomodation accomodation, Integer minPrice, boolean reservationAble) {
+    public static AccomodationCardResponseDto toDto(Accomodation accomodation, Integer minPrice, boolean reservationAvailable) {
         int likeCount = 0;
         if (accomodation.getLikes() != null) {
             likeCount = accomodation.getLikes().size();
         }
 
-        return AccomodationCardResponseDto.builder()
-                .id(accomodation.getId())
-                .name(accomodation.getName())
-                .description(accomodation.getDescription())
-                .postalCode(accomodation.getPostalCode())
-                .address(accomodation.getAddress())
-                .parkingAvailable(accomodation.getParkingAvailable())
-                .cookingAvailable(accomodation.getCookingAvailable())
-                .checkIn(accomodation.getCheckIn())
-                .checkOut(accomodation.getCheckOut())
-                .category(accomodation.getCategory().getName())
-                .minPrice(minPrice)
-                .reservationAvailable(reservationAble)
-                .likeCount(likeCount)
-                .build();
-    }
+        List<String> imageUrls = accomodation.getImages().stream()
+            .map(AccomodationImage::getImgUrl)
+            .collect(Collectors.toList());
 
+        return AccomodationCardResponseDto.builder()
+            .id(accomodation.getId())
+            .name(accomodation.getName())
+            .description(accomodation.getDescription())
+            .postalCode(accomodation.getPostalCode())
+            .address(accomodation.getAddress())
+            .parkingAvailable(accomodation.getParkingAvailable())
+            .cookingAvailable(accomodation.getCookingAvailable())
+            .checkIn(accomodation.getCheckIn())
+            .checkOut(accomodation.getCheckOut())
+            .category(accomodation.getCategory().getName())
+            .minPrice(minPrice)
+            .reservationAvailable(reservationAvailable)
+            .likeCount(likeCount)
+            .imageUrls(imageUrls)
+            .build();
+    }
 }
