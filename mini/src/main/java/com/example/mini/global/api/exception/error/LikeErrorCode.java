0ea272b0ba27155d9d96a6c5498ac430f79be868
leeshinbi <tlsql6831@naver.com>
2024-07-20 21:01:07 +0900

    feat : 좋아요 기능에 redis cache, batch 작업 추가

--- mini/src/main/java/com/example/mini/global/api/exception/error/LikeErrorCode.java
+++ mini/src/main/java/com/example/mini/global/api/exception/error/LikeErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 public enum LikeErrorCode implements ErrorCode {
 
   MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
-  ACCOMODATION_NOT_FOUND(HttpStatus.NOT_FOUND, "숙소를 찾을 수 없습니다");
+  ACCOMODATION_NOT_FOUND(HttpStatus.NOT_FOUND, "숙소를 찾을 수 없습니다"),
+  LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 찾을 수 없습니다");
 
   private final HttpStatus code;
   private final String info;
---
+++ mini/src/main/java/com/example/mini/global/redis/CacheService.java
@@ -0,0 +1,30 @@
+package com.example.mini.global.redis;
+
+import java.util.concurrent.TimeUnit;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CacheService {
+
+	private RedisTemplate<String, Object> redisTemplate;
+
+	private static final String LIKE_KEY_PREFIX = "like::";
+
+	public void cacheLikeStatus(Long memberId, Long accomodationId, boolean isLiked) {
+		String key = LIKE_KEY_PREFIX + memberId + "::" + accomodationId;
+		redisTemplate.opsForValue().set(key, isLiked, 1, TimeUnit.HOURS); // 1시간 TTL
+	}
+
+	public Boolean getLikeStatus(Long memberId, Long accomodationId) {
+		String key = LIKE_KEY_PREFIX + memberId + "::" + accomodationId;
+		return (Boolean) redisTemplate.opsForValue().get(key);
+	}
+
+	public void evictLikeStatus(Long memberId, Long accomodationId) {
+		String key = LIKE_KEY_PREFIX + memberId + "::" + accomodationId;
+		redisTemplate.delete(key);
+	}
+}
